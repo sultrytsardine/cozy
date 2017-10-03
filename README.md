ba0f31c4abb00821944b6606e4b439388f7bc05e
wtty-fool <wtty.fool@gmail.com>
2017-10-04 00:32:58 +0200

    plugin mechanism

--- README.md
+++ README.md
@@ -8,13 +8,3 @@ Inspired by oh-my-zsh's first commit (basically a bunch of .zsh scrripts with sa
 + helps you keep track of changes and update
 + supports your own plugins and extensions!
 
-## Plugins
-found in `.basher/plugins`, each plugin has at least 3 scripts: `install.sh`, `uninstall.sh`, and `<plugin_name>.sh`, where `<plugin_name>` is the same as it's directory's name
-
-## Properties
-+ default location: `$HOME/.basher`
-+ main script: `$BASHER/basher.sh`
-+ fresh install? do `./$HOME/.basher/install.sh`
-+ plugin management: `$BASHER/plugins.sh`
-+ plugin location: `$BASHER/plugins/`
-
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+source "./functions.sh"
 
 #
 #   Basher
@@ -15,44 +16,50 @@ BASHER_LOCATION="$HOME/.basher"
 if [ -d $BASHER_LOCATION ]; then
     # TODO - prompt if user wants a pure reinstall to happen via git
     echo "Directory '$BASHER_LOCATION' already exists!"
-    exit 1
+    #exit 1
 else
     #
     #   BASHER
     #
     echo "Installing Basher at '$BASHER_LOCATION'..."
     # update packages & install necessary ones
-    apt update -y &> /dev/null || (echo Error during apt update! && exit 1)
-    apt install -y git &> /dev/null || (echo Error installing git! && exit 1)
-    mkdir -p $BASHER_LOCATION
-    git clone https://github.com/wtty-fool/basher $BASHER_LOCATION
-    chmod +x $BASHER_LOCATION
+    #apt update -y &> /dev/null || (echo Error during apt update! && exit 1)
+    #apt install -y git &> /dev/null || (echo Error installing git! && exit 1)
+    #mkdir -p $BASHER_LOCATION
+    #git clone https://github.com/wtty-fool/basher $BASHER_LOCATION
+    #chmod +x $BASHER_LOCATION
+
+    # Plugins
+    echo Installing plugins...
+    for path in plugins/*/; do
+       plugin $path
+    done
 
     # TODO - iterate all plugins, running installations for them
-    source "$BASHER_LOCATION/plugins/vim/install.sh"
-   
+    # source "$BASHER_LOCATION/plugins/vim/install.sh"
+
 
 
     #
     # GitHub twofactor
     #
-    
+
 
     #
     # Git
     #
-    
+
 
     #
     # Colors
     #
-   
+
 
 
     #
     # .BASHRC
     #
     # everything went smoothly, add new line to .bashrc
-    echo "export BASHER=$BASHER_LOCATION" >> "$HOME/.bashrc"
-    echo "source $BASHER/.basherrc" >> "$HOME/.bashrc"
+    #echo "export BASHER=$BASHER_LOCATION" >> "$HOME/.bashrc"
+    #echo "source $BASHER/.basherrc" >> "$HOME/.bashrc"
 fi

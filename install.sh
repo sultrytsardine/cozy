187a1bed1fce08fcc39527994daffb8169bc0bac
wtty-fool <wtty.fool@gmail.com>
2017-10-03 23:14:46 +0200

    Big changes!

--- install.sh
+++ install.sh
@@ -22,8 +22,8 @@ else
     #
     echo "Installing Basher at '$BASHER_LOCATION'..."
     # update packages & install necessary ones
-    apt update &> /dev/null || (echo Error during apt update! && exit 1)
-    apt install git &> /dev/null || (echo Error installing git! && exit 1)
+    apt update -y &> /dev/null || (echo Error during apt update! && exit 1)
+    apt install -y git &> /dev/null || (echo Error installing git! && exit 1)
     mkdir -p $BASHER_LOCATION
     git clone https://github.com/wtty-fool/basher $BASHER_LOCATION
     chmod +x $BASHER_LOCATION
@@ -34,13 +34,13 @@ else
     #
     echo "Installing vim..."
     # VIMRC setup
-    apt install vim &> /dev/null || (echo Failed installing vim! && exit 1)
+    apt install -i vim &> /dev/null || (echo Failed installing vim! && exit 1)
     # if .vimrc exists, rename it
     if [ -f "$HOME/.vimrc"]; then
         echo 'Backing up .vimrc -> .vimrc.bak...'
         mv "$HOME/.vimrc" "$HOME/.vimrc.bak"
     fi
-    ln --symbolic "$BASHER_LOCATION/.vimrc" "$HOME/.vimrc"
+    ln --symbolic "$BASHER_LOCATION/vimrc" "$HOME/.vimrc"
     # Vundle
     echo "Installing Vundle..."
     git clone https://github.com/VundleVim/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"
@@ -48,10 +48,37 @@ else
     # if YouCompleteMe is installed, need to recompile
     if [ -d "$HOME/.vim/bundle/YouCompleteMe" ]; then;
         echo "Recompiling YouCompleteMe libraries..."
-        apt install build-essential cmake &> /dev/null || (echo Failed installing cmake and build-essential! && exit 1)
+        apt install -y build-essential cmake &> /dev/null || (echo Failed installing cmake and build-essential! && exit 1)
         python "$HOME/.vim/bundle/YouCompleteMe/install.py" $> /dev/null || (echo Compilation failed! && exit 1)
     fi
 
+
+    #
+    # GitHub twofactor
+    #
+    echo Setting up GitHub two-factor access...
+    apt install gcc libgnome-keyring-dev -y &> /dev/null || (echo Failed installing gnome-keyring! && exit 1)
+    make --directory=/usr/share/doc/git/contrib/credential/gnome-keyring &> /dev/null || (echo Failed building gnome-keyring! && exit 1)
+    git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
+
+    #
+    # Git
+    #
+    echo Configuring git to your liking...
+    git config --global user.name wtty-fool
+    git config --global user.email wtty.fool@gmail.com
+    git config --global core.editor vim
+    echo Setting git aliases...
+    git config --global alias.last 'log -1 --stat'
+    git config --global alias.cp 'cherry-pick'
+    git config --global alias.co 'checkout'
+    git config --global alias.cl 'clone'
+    git config --global alias.ci 'commit'
+    git config --global alias.st 'status -sb'
+    git config --global alias.br 'branch'
+    git config --global alias.unstage 'reset HEAD --'
+    git config --global alias.dc 'diff --cached'
+    git config --global alias.lg 'log --graph --pretty format:"%Cred%h%Creset -%C(yel%Cblue<%an>%Creset" --abbrev-commit --date relative --all'
 
 
     #

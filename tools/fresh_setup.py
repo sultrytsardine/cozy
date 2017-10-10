9d7388184c406e26833a20f9caa8150550187004
Jakub Soboń <wtty.fool@gmail.com>
2017-10-10 21:49:05 +0200

    Update fresh_setup.py

--- tools/fresh_setup.py
+++ tools/fresh_setup.py
@@ -5,31 +5,36 @@ import json
 
 
 def execute(cmd):
-	with open(os.devnull, 'w') as nl:
-            _ = subprocess.Popen(cmd.split(), stdout=nl, stderr=nl)
-
+    p = subprocess.Popen(cmd.split())
+    p.wait()
 
 if __name__ == '__main__':
+    basher_home = os.environ.get('BASHER_HOME', '~/.basher')
+    path = basher_home + '/tools/fresh_setup.config'
+    print(path)
     try:
-        with open('fresh_setup.config', 'r') as f:
+        with open(path, 'r') as f:
             conf = json.loads(f.read())
     except:
-        print('Error while reading config')
-
+       print('Error while reading config')
+       exit(1)
+    
     # TODO: mandatory. optional?
-	exec('sudo apt install software-properties-common')
-	for ppa in conf['ppa']:
-		print('+ %s' % ppa)
-		cmd = 'sudo apt-add-repository %s' % ppa
-		execute(cmd)
-		
+    execute('sudo apt update')
+    execute('sudo apt install software-properties-common -q -y')
+    for ppa in conf['ppa']:
+        print('+ %s' % ppa)
+        cmd = 'sudo apt-add-repository %s -q -y' % ppa
+        execute(cmd)
+    execute('sudo apt update')
+
     for package in conf['mandatory']:
         print('. %s' % package)
         cmd = 'sudo apt install %s -q -y' % package
         execute(cmd)
-		
+
     for package in conf['optional']:
         print('? %s' % package)
-		cmd = 'sudo apt install %s -q -y' % package
+        cmd = 'sudo apt install %s -q -y' % package
         execute(cmd)
-
+    execute('sudo apt upgrade')
